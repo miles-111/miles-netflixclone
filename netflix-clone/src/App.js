3000ec8cd333480594403a241c2886f4bb09e83f
roelbonagua-lang <roelbonagua@gmail.com>
2026-03-31 19:20:05 +0800

    add scrollbar, movie images

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -6,9 +6,15 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <h1>KAMUSTA MGA WALANG BITAW!!</h1>
+      <h1>Barurot.tv</h1>
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
+import './Row.css';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
@@ -23,10 +26,12 @@ function Row({ title, fetchUrl }) {
         <h2/>{title}<h2/>
 
         <div className="row__posters">
-            {/* several row__posters*/}
-
             {movies.map(movie => (
-                <img src={movie.poster_path} alt={movie.name}/>
+                <img
+                key={movie.id}
+                className="row__poster"
+                src={`${base_url}${movie.poster_path}`} 
+                alt={movie.name} />
             ))}
         </div>
         {/* container -> posters */}
